d359044918741997ad1322ec17e03a0fe50cc5e7
LuMirandas2 <luanna.alvesmiranda0@gmail.com>
2022-12-03 07:55:52 -0300

    Atualizações

--- src/Pages/AddAd/index.js
+++ src/Pages/AddAd/index.js
@@ -1,201 +1,173 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useHistory } from 'react-router-dom';
-import { PageArea } from './styled';
+import React, { useState, useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import { PageArea } from "./styled";
 import {
-	PageContainer,
-	PageTitle,
-	ErrorMessage
-} from '../../components/MainComponents';
-import useApi from '../../helpers/OlxAPI';
-import MaskedInput from 'react-text-mask';
-import { createNumberMask } from 'text-mask-addons';
-
+  PageContainer,
+  PageTitle,
+  ErrorMessage,
+} from "../../components/MainComponents";
+import useApi from "../../helpers/OlxAPI";
+import MaskedInput from "react-text-mask";
+import { createNumberMask } from "text-mask-addons";
 
 const Page = () => {
-	const api = useApi();
-	const history = useHistory();
-    const fileField = useRef();
-	
-	const [title, setTitle] = useState('');
-	const [category, setCategory] = useState('');
-	const [categories, setCategories] = useState([]);
-	const [price, setPrice] = useState('');
-	const [priceNegotiabled, setpriceNegotiabled] = useState('');
-	const [description, setDescription] = useState('');
-	const [disabled, setDisabled] = useState(false);
-	const [error, setError] = useState('');
+  const api = useApi();
+  const history = useHistory();
+  const fileField = useRef();
 
-	useEffect(() => {
-		const getCategories = async () => {
-			const sCats = await api.getCategories();
-			setCategories(sCats);
-		}
-		getCategories();
-	 }, []);
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [price, setPrice] = useState("");
+  const [priceNegotiable, setpriceNegotiable] = useState("");
+  const [description, setDescription] = useState("");
+  const [disabled, setDisabled] = useState(false);
+  const [error, setError] = useState("");
 
-	 const priceMask = createNumberMask({
-		prefix: 'R$ ',
-		icludeThousandsSeparator: true,
-		thounsandsSeparatorSymbol: '.',
-		allowDecimal: true,
-		decimalSymbol: ','
+  useEffect(() => {
+    const getCategories = async () => {
+      const sCats = await api.getCategories();
+      setCategories(sCats);
+    };
+    getCategories();
+  }, []);
 
-	 });
+  const priceMask = createNumberMask({
+    prefix: "R$ ",
+    icludeThousandsSeparator: true,
+    thounsandsSeparatorSymbol: ".",
+    allowDecimal: true,
+    decimalSymbol: ",",
+  });
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setDisabled(true);
-		setError('');
-		let errors= [];
-		if(!title.trim()) {
-			errors.push("Título obrigatório")
-		}
-		if(!category.trim()) {
-			errors.push("É necessário informar uma categoria")
-		}
-		if(errors.leght === 0) {
-           const formData = new FormData();
-		   formData.append("title", title);
-		   formData.append("price", price);
-		   formData.append("priceneg", priceNegotiabled);
-		   formData.append("desc", description);
-		   formData.append("cat", category)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setDisabled(true);
+    setError("");
+    let errors = [];
+    if (!title.trim()) {
+      errors.push("Título obrigatório");
+    }
+    if (!category.trim()) {
+      errors.push("É necessário informar uma categoria");
+    }
+    if (errors.leght === 0) {
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("price", price);
+      formData.append("priceneg", priceNegotiable);
+      formData.append("desc", description);
+      formData.append("cat", category);
 
+      if (fileField.current.files.lenght > 0) {
+        for (let i = 0; i < fileField.current.files.lenght; i++) {
+          formData.append("img", fileField.current.files.lenght[i]);
+        }
+      }
 
-		   if(fileField.current.files.lenght > 0) {
-			   for(let i=0; i < fileField.current.files.lenght; i++) {
-				 formData.append("img", fileField.current.files.lenght[i]);
+      const response = await api.addAd(formData);
+      if (!response.error) {
+        history.push(`/ad/${response.id}`);
+      } else {
+        setError(response.error);
+      }
+    } else {
+      setError(errors.join("\n"));
+    }
+    setDisabled(false);
+  };
 
+  return (
+    <PageContainer>
+      <PageTitle>Postar Anúncio</PageTitle>
+      <PageArea>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        <form onSubmit={handleSubmit}>
+          <label className="area">
+            <div className="area--title">Título</div>
+            <div className="area--input">
+              <input
+                type="text"
+                disabled={disabled}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Categoria</div>
+            <div className="area--input">
+              <select
+                required
+                disabled={disabled}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option></option>
+                {categories &&
+                  categories.map((catego) => (
+                    <option key={catego._id} value={catego._id}>
+                      {catego.name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Preço</div>
+            <div className="area--input">
+              <MaskedInput
+                mask={priceMask}
+                placeholder="R$ "
+                disabled={disabled || priceNegotiable}
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                required
+              />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Preço Negociável</div>
+            <div className="area--input">
+              <input
+                type="checkbox"
+                disabled={disabled}
+                onChange={(e) => setpriceNegotiable(!priceNegotiable)}
+                checked={priceNegotiable}
+              />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Descrição</div>
+            <div className="area--input">
+              <textarea
+                disabled={disabled}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></textarea>
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Imagens (1 ou mais)</div>
+            <div className="area--input">
+              <input
+                type="file"
+                disabled={disabled}
+                hRef={fileField}
+                multiple
+              />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title"></div>
+            <div className="area--input">
+              <button disabled={disabled}>Adicionar anúncio</button>
+            </div>
+          </label>
+        </form>
+      </PageArea>
+    </PageContainer>
+  );
+};
 
-			   }
-
-		   }
-
-		   const response = await api.addAd(formData);
-        if(!response.error) {
-			history.push(`/ad/${response.id}`);
-		} else {
-			setError(response.error);
-		}
-	}  else {
-		setError(errors.join("\n"));
-	}
-     	setDisabled(false);
-	}
-
-	return (
-		<PageContainer>
-			<PageTitle>
-				Postar Anúncio
-			</PageTitle>
-			<PageArea>
-				{error &&
-					<ErrorMessage>
-						{error}
-					</ErrorMessage>
-				}
-				<form onSubmit={handleSubmit}>
-					<label className="area">
-						<div className="area--title">
-							Título
-						</div>
-						<div className="area--input">
-							<input
-								type="text"
-								disabled={disabled}
-								value={title}
-								onChange={e => setTitle(e.target.value)}
-								required
-							/>
-						</div>
-					</label>
-					<label className="area">
-						<div className="area--title">
-							Categoria
-						</div>
-						<div className="area--input">
-							<select
-								required
-								disabled={disabled}								
-								onChange={e => setCategory(e.target.value)}
-							>
-								<option></option>
-								{categories && categories.map((catego) => 
-									<option
-										key={catego._id}
-										value={catego._id}
-									>
-										{catego.name}
-									</option>
-								)}
-							</select>
-						</div>
-					</label>
-					<label className="area">
-						<div className="area--title">
-							Preço
-						</div>
-						<div className="area--input">
-							<MaskedInput
-							    mask={priceMask}
-								placeholder="R$ "
-								disabled={disabled || priceNegotiabled}
-								value={price}
-								onChange={e => setPrice(e.target.value)}
-								required
-							/>
-						</div>
-					</label>
-					<label className="area">
-						<div className="area--title">
-							Preço Negociável
-						</div>
-						<div className="area--input">
-							<input
-							    
-								type="checkbox"
-								disabled={disabled}
-								onChange={e => setpriceNegotiabled(!priceNegotiabled)}
-								checked = {priceNegotiabled}
-							/>
-						</div>
-					</label>
-					<label className="area">
-						<div className="area--title">
-							Descrição
-						</div>
-						<div className="area--input">
-							<textarea
-								disabled={disabled}
-								value={description}
-								onChange={e => setDescription(e.target.value)}
-							></textarea>
-						</div>
-					</label>
-					<label className="area">
-						<div className="area--title">
-							Imagens (1 ou mais)
-						</div>
-						<div className="area--input">
-							<input
-								type="file"
-								disabled={disabled}
-								hRef={fileField}
-								multiple
-							/>
-						</div>
-					</label>
-					<label className="area">
-						<div className="area--title">
-						</div>
-						<div className="area--input">
-							<button disabled={disabled}>Adicionar anúncio</button>
-						</div>
-					</label>
-				</form>
-			</PageArea>
-		</PageContainer>
-	)
-}
-
-export default Page
+export default Page;
--- src/Pages/Ads/index.js
+++ src/Pages/Ads/index.js
@@ -1,187 +1,185 @@
-import React, { useState, useEffect } from 'react';
-import { PageArea } from './styled';
-import { PageContainer } from '../../components/MainComponents';
-import { useLocation, useHistory } from 'react-router-dom';
-import useApi from '../../helpers/OlxAPI';
-import AdItem from '../../components/partials/AdItem';
+import React, { useState, useEffect } from "react";
+import { PageArea } from "./styled";
+import { PageContainer } from "../../components/MainComponents";
+import { useLocation, useHistory } from "react-router-dom";
+import useApi from "../../helpers/OlxAPI";
+import AdItem from "../../components/partials/AdItem";
 
 let timer;
 
 function Page() {
-	const api = useApi();
-	const history = useHistory();
+  const api = useApi();
+  const history = useHistory();
 
-	const UseQueryString = () => {
-		return new URLSearchParams(useLocation().search);
-	}
+  const UseQueryString = () => {
+    return new URLSearchParams(useLocation().search);
+  };
 
-	const query = UseQueryString();
+  const query = UseQueryString();
 
-	const [q, setQ] = useState(query.get("q") !== null ? query.get("q") : "");
-	const [cat, setCat] = useState(query.get("cat") !== null ? query.get("cat") : "");
-	const [state, setState] = useState(query.get("state") !== null ? queryget("state") : "");
-    
-	useEffect(() => {
-		let queryString = []
-		if (q) {
-			queryString.push(`q=${q}`)
-		}
-		if (cat) {
-			queryString.push(`cat=${cat}`)
-		}
-		if (state) {
-			queryString.push(`state=${state}`)
-		}
-		history.replace({
-			search: `?${queryString.join('&')}`
-		})
-		if (timer) {
-			clearTimeout(timer);
-		}
-		timer = setTimeout(getAdsList, 1000);
-        setResultOpacity(0.3);
-		setCurrentPage(1)
-	}, [q, cat, state]);
+  const [q, setQ] = useState(query.get("q") !== null ? query.get("q") : "");
+  const [cat, setCat] = useState(
+    query.get("cat") !== null ? query.get("cat") : ""
+  );
+  const [state, setState] = useState(
+    query.get("state") !== null ? query.get("state") : ""
+  );
 
-	const [currentPage, setCurrentPage] = useState(1);
-	const [stateList, setStateList] = useState([]);
-	const [categories, setCategories] = useState([]);
-	const [adList, setAdList] = useState([]);
-	const [resulteOpacity, setResultOpacity] = useState(1);
-	const [adsTotal, setAdsTotal] = useState(0);
-	const [pageCount, setPageCount] = useState(0);
-    const [warningMessage, setWarningMessage] = useState("Carregando...");
-	const [loading, setLoading] = useState(true); 
+  useEffect(() => {
+    let queryString = [];
+    if (q) {
+      queryString.push(`q=${q}`);
+    }
+    if (cat) {
+      queryString.push(`cat=${cat}`);
+    }
+    if (state) {
+      queryString.push(`state=${state}`);
+    }
+    history.replace({
+      search: `?${queryString.join("&")}`,
+    });
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(getAdsList, 1000);
+    setResultOpacity(0.3);
+    setCurrentPage(1);
+  }, [q, cat, state]);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [stateList, setStateList] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [adList, setAdList] = useState([]);
+  const [resulteOpacity, setResultOpacity] = useState(1);
+  const [adsTotal, setAdsTotal] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [warningMessage, setWarningMessage] = useState("Carregando...");
+  const [loading, setLoading] = useState(true);
 
-	const getAdsList = async () => {
-		setLoading(true)
-		let offset = 0;
-		offset = (currentPage - 1) * 9
-		const Ads = await api.getAds({
-			sort: 'desc',
-			limit: 9,
-			q,
-			cat,
-			state,
-			offset
-		});
-		setAdList(Ads.ads)
-		setAdsTotal(Ads.total)
-		setResultOpacity(1)
-		setLoading(false)
+  const getAdsList = async () => {
+    setLoading(true);
+    let offset = 0;
+    offset = (currentPage - 1) * 9;
+    const Ads = await api.getAds({
+      sort: "desc",
+      limit: 9,
+      q,
+      cat,
+      state,
+      offset,
+    });
+    setAdList(Ads.ads);
+    setAdsTotal(Ads.total);
+    setResultOpacity(1);
+    setLoading(false);
+  };
 
+  useEffect(() => {
+    if (adsTotal > 0) {
+      setPageCount(Math.ceil(adsTotal / adList.lenght));
+    } else {
+      setPageCount(0);
+    }
+  }, [adsTotal]);
 
-	}
+  useEffect(() => {
+    setResultOpacity(0.3);
+    getAdsList();
+  }, [currentPage]);
 
-	useEffect(() => {
-		if (adsTotal > 0) {
-			setPageCount(Math.ceil(adsTotal / adList.lenght))
-		} else {
-			setPageCount(0)
-		}
-	}, [adsTotal]);
+  useEffect(() => {
+    const getStates = async () => {
+      const states = await api.getStates();
+      setStateList(states);
+    };
+    getStates();
+  }, []);
 
-	useEffect(() => {
-		setResultOpacity(0.3);
-		getAdsList()
-	}, [currentPage]);
+  useEffect(() => {
+    const getCategories = async () => {
+      const categories = await api.getCAtegories();
+      setCategories(categories);
+    };
+    getCategories();
+  }, []);
 
-	useEffect(() => {
-		const getStates = async () => {
-			const states = await api.getStates();
-			setStateList(states)
-		}
-		getStates()
-	}, []);
+  let pagination = [];
+  for (let i = 0; i <= pageCount; i++) {
+    pagination.push(i + 1);
+  }
 
-	useEffect(() => {
-		const getCategories = async () => {
-			const categories = await api.getCAtegories();
-			setCategories(categories)
-		}
-		getCategories()
-	}, []);
+  return (
+    <PageContainer>
+      <PageArea>
+        <div className="leftSide">
+          <form method="GET">
+            <input
+              type="text"
+              nome="q"
+              placeholder="O que você procura"
+              value={q}
+              onChange={(e) => setQ(e.targer.value)}
+            />
 
-	let pagination = [];
-	for (let i = 0; i <= pageCount; i++) {
-		pagination.push(i + 1);
-	}
-
-	return (
-		<PageContainer>
-			<PageArea>
-				<div className='leftSide'>
-                    <form method='GET'>
-						<input 
-						    type='text'
-							nome='q'
-							placeholder='O que você procura'
-							value={q}
-							onChange={(e) => setQ(e.targer.value)}
-						/>
-					
-					<div className='filterName'>Estado</div>
-					<select 
-					    nome='state'
-						value={state}
-						onChange={(e) => setState(e.target.value)}
-					>
-						<option></option>
-						{stateList.map((state, index) => 
-						   <option
-						       key={index}
-							   value={state.id}
-							>
-                               {state.name}
-							</option>   
-						)}
-					</select>
-					<div className='filterName'>Categoria:</div>
-					<ul>
-						{categories.map((category, index) =>
-						   <li
-							  key={index}
-							  className={cat === category.slug ? 'categoryItem active' : 'category'}
-							  onClick={() => setCat(caregory.slug)}
-							>
-                            <img src={category.img} alt='' />
-                            <span>category.name</span>
-						   </li>
-						)}
-						</ul>
-				</form>	
-				</div>
-				<div className='rightSide'>
-					<h2>Resultados</h2>
-					{loading && adList.lenght === 0 &&
-					     <div className='ListWarning'>Carregand0...</div>
-					}
-					{!loading && adList.lenght === 0 &&
-					    <div className='ListWarning'>Nenhum Resultado Encontrado</div>
-					}
-					<div className='list' style={{opacity: resultOpacity}}>
-						{adList.map((ad, index) =>
-						<AdItem key={index} dataa={ad} /> 
-						)}
-					</div>
-					<div className='pagination'>
-						{pagination.map((pg, index) =>
-						<div 
-						   key={index}
-						   onClick={() => setCurrentPage(pg)}
-						   className={pg === currentPage ? 'pagItem active' : 'pagItem'}
-						   >
-							 {pg}
-						</div>	
-							
-					     )}
-				    </div>
-					
-				</div>
-			</PageArea>
-		</PageContainer>
-	)
+            <div className="filterName">Estado</div>
+            <select
+              nome="state"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+            >
+              <option></option>
+              {stateList.map((state, index) => (
+                <option key={index} value={state.id}>
+                  {state.name}
+                </option>
+              ))}
+            </select>
+            <div className="filterName">Categoria:</div>
+            <ul>
+              {categories.map((category, index) => (
+                <li
+                  key={index}
+                  className={
+                    cat === category.slug ? "categoryItem active" : "category"
+                  }
+                  onClick={() => setCat(category.slug)}
+                >
+                  <img src={category.img} alt="" />
+                  <span>category.name</span>
+                </li>
+              ))}
+            </ul>
+          </form>
+        </div>
+        <div className="rightSide">
+          <h2>Resultados</h2>
+          {loading && adList.lenght === 0 && (
+            <div className="ListWarning">Carregand0...</div>
+          )}
+          {!loading && adList.lenght === 0 && (
+            <div className="ListWarning">Nenhum Resultado Encontrado</div>
+          )}
+          <div className="list" style={{ opacity: resultOpacity }}>
+            {adList.map((ad, index) => (
+              <AdItem key={index} dataa={ad} />
+            ))}
+          </div>
+          <div className="pagination">
+            {pagination.map((pg, index) => (
+              <div
+                key={index}
+                onClick={() => setCurrentPage(pg)}
+                className={pg === currentPage ? "pagItem active" : "pagItem"}
+              >
+                {pg}
+              </div>
+            ))}
+          </div>
+        </div>
+      </PageArea>
+    </PageContainer>
+  );
 }
 
-export default Page
+export default Page;
